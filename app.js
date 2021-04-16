b100383e63e3ae4216f82dc40c73e5d9899cbe03
Michael Ha <michael.ha90@gmail.com>
2021-04-16 17:59:58 +0200

    Test

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 
-// Start Express application
+// Start Express application -
 const app = express();
 
 // setup pug view template
